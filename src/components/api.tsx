d076fdc47ba530179df58efb5241a442acf04994
Kirill Skakov <kirill.skakov@gmail.com>
2024-08-18 21:41:58 +0900

    https settings change

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,4 +1,4 @@
 export const weatherApi = 'https://api.openweathermap.org/data/2.5/weather';
 export const forecastApi = 'https://api.openweathermap.org/data/2.5/forecast';
-export const citySearchApi = 'http://api.openweathermap.org/geo/1.0/direct';
-export const weatherIconApi = 'http://openweathermap.org/img/wn/';
+export const citySearchApi = 'https://api.openweathermap.org/geo/1.0/direct';
+export const weatherIconApi = 'https://openweathermap.org/img/wn/';
